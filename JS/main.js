533cb6ea76cf8c93ca51b27165edd382657e609f
lankeycode <michael.moussaoui@gmail.com>
2021-12-26 23:08:15 +0100

    #2 modified

--- JS/main.js
+++ JS/main.js
@@ -3,29 +3,27 @@ const mouse = document.querySelector('.mouse')
 const toogler = document.querySelector('.navbar-toggler')
 const icon = document.querySelector('.navbar-toggler-icon')
 const menu = document.querySelector('.togglerMenu')
-const link = document.querySelector('.nav-link')
+const link = document.querySelector('.navbar-link')
 const myName = document.querySelector('.myName')
+const presentation = document.querySelector('.presentation')
+const navbarTop = document.querySelector('.navbar-nav-top')
+const linkTop = document.querySelector('.navbar-link-top')
+const section = document.querySelector('.section')
 
 
 
 
 
-window.addEventListener('mousemove', (e) => {
 
-    // mouse.style.left = e.pageX + 'px'
-    // mouse.style.top = e.pageY + 'px'
-
-    // mouse.style.left = e.pageX + 'px'
-    // mouse.style.top = e.pageY + 'px'
-
-})
-
+// Animation Menu au click
 toogler.addEventListener('click', () => {
     body.classList.toggle('active')
     body.classList.toggle('background')
     icon.classList.toggle('active')
     menu.classList.toggle('active')
     myName.classList.toggle('active')
+    presentation.classList.toggle('active')
+
 
 })
 
@@ -36,13 +34,38 @@ toogler.addEventListener('click', () => {
 window.addEventListener('mousemove', (e) => {
     const speed = myName.getAttribute('data-speed')
 
-    const x = (window.innerWidth - e.pageX * speed) / 100
-    const y = (window.innerHeight - e.pageY * speed) / 100
+    const x = (window.innerWidth + e.pageX * speed) / 100
+    const y = (window.innerHeight + e.pageY * speed) / 100
+    const x1 = (window.innerWidth - e.pageX * speed) / 100
+    const y1 = (window.innerHeight - e.pageY * speed) / 100
 
     myName.style.transform = `translateX(${x}px) translateY(${y}px)`
+    presentation.style.transform = `translateX(${x1}px) translateY(${y1}px)`
 
     mouse.style.left = e.pageX + 'px'
     mouse.style.top = e.pageY + 'px'
 
 })
+// Afficher la barre de navigation au scroll
+window.addEventListener('scroll', (event) => {
 
+    if (scrollY > 500) {
+        navbarTop.style.top = "0px"
+
+    } else {
+        navbarTop.style.top = "-100px"
+    }
+})
+
+// Modofier l'opacité au scroll
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 501) {
+        section.style.opacity = 0.5
+    } else {
+        section.style.opacity = 1
+    }
+
+
+
+})
