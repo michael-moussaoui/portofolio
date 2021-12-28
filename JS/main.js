914a8792fc2f65f0ec31e64b4f9fd00705e41cb6
lankeycode <michael.moussaoui@gmail.com>
2021-12-29 00:46:51 +0100

    #3 modified

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,25 @@
 const body = document.querySelector('body')
-const mouse = document.querySelector('.mouse')
+const mouse = document.querySelector('#mouse')
 const toogler = document.querySelector('.navbar-toggler')
 const icon = document.querySelector('.navbar-toggler-icon')
 const menu = document.querySelector('.togglerMenu')
-const link = document.querySelector('.navbar-link')
+const link = document.querySelector('.nav-link')
 const myName = document.querySelector('.myName')
 const presentation = document.querySelector('.presentation')
 const navbarTop = document.querySelector('.navbar-nav-top')
 const linkTop = document.querySelector('.navbar-link-top')
+const navbarNav = document.querySelector('.navbar-nav')
+const arowUp = document.querySelector('.arowUp')
 const section = document.querySelector('.section')
 
 
 
 
+
+window.addEventListener('mousemove', (e) => {
+    mouse.style.left = e.pageX + 'px'
+    mouse.style.top = e.pageY + 'px'
+})
 
 
 // Animation Menu au click
@@ -21,6 +28,7 @@ toogler.addEventListener('click', () => {
     body.classList.toggle('background')
     icon.classList.toggle('active')
     menu.classList.toggle('active')
+    navbarNav.classList.toggle('clicked')
     myName.classList.toggle('active')
     presentation.classList.toggle('active')
 
@@ -32,6 +40,8 @@ toogler.addEventListener('click', () => {
 // })
 
 window.addEventListener('mousemove', (e) => {
+
+
     const speed = myName.getAttribute('data-speed')
 
     const x = (window.innerWidth + e.pageX * speed) / 100
@@ -42,30 +52,47 @@ window.addEventListener('mousemove', (e) => {
     myName.style.transform = `translateX(${x}px) translateY(${y}px)`
     presentation.style.transform = `translateX(${x1}px) translateY(${y1}px)`
 
-    mouse.style.left = e.pageX + 'px'
-    mouse.style.top = e.pageY + 'px'
-
 })
 // Afficher la barre de navigation au scroll
 window.addEventListener('scroll', (event) => {
 
     if (scrollY > 500) {
         navbarTop.style.top = "0px"
+        navbarTop.classList.add('active')
+        arowUp.style.visibility = 'visible'
 
     } else {
         navbarTop.style.top = "-100px"
+        navbarTop.classList.remove('showArow')
+        arowUp.style.visibility = 'hidden'
+
     }
 })
 
-// Modofier l'opacité au scroll
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 501) {
-        section.style.opacity = 0.5
-    } else {
-        section.style.opacity = 1
-    }
-
-
+// hover navlink 
+link.addEventListener('mouseover', e => {
+    console.log('hover');
+    // link.classList.add('hover')
 
 })
+
+// Modifier l'opacité au scroll
+
+// window.addEventListener('scroll', () => {
+//     let height = document.scrollTop
+//     if (height > 501) {
+//         section.style.background = blue
+//     }
+//     //  else {
+//     //     section.style.background = green
+//     // }
+
+// })
+
+// $(window).scroll(function () {
+//     var height = $(window).scrollTop();
+
+//     if (height > some_number) {
+//         // do something
+//     }
+// });
